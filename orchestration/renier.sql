a6cb8e444e932b9d6cbb465195f0b537adee7747
Renier-Rousseau <renier@hisp.org>
2017-09-29 15:51:52 +0200

    adding serial

--- orchestration/renier.sql
+++ orchestration/renier.sql
@@ -17,7 +17,6 @@
 -- object: public.hosts | type: TABLE --
 -- DROP TABLE IF EXISTS public.hosts CASCADE;
 CREATE TABLE public.hosts(
-	hosts_pk smallint NOT NULL,
 	host_name varchar(255) NOT NULL,
 	ip_external varchar(15),
 	ip_internal varchar(15),
@@ -31,7 +30,7 @@ ALTER TABLE public.hosts OWNER TO postgres;
 -- object: public.db_server | type: TABLE --
 -- DROP TABLE IF EXISTS public.db_server CASCADE;
 CREATE TABLE public.db_server(
-	db_server_pk smallint NOT NULL,
+	db_server_pk serial NOT NULL,
 	fk_hosts_pk smallint NOT NULL,
 	port integer,
 	CONSTRAINT db_server_pk PRIMARY KEY (db_server_pk)
@@ -44,7 +43,7 @@ ALTER TABLE public.db_server OWNER TO postgres;
 -- object: public.db | type: TABLE --
 -- DROP TABLE IF EXISTS public.db CASCADE;
 CREATE TABLE public.db(
-	db_pk smallint NOT NULL,
+	db_pk serial NOT NULL,
 	db_server_fk smallint NOT NULL,
 	db_name varchar(255),
 	CONSTRAINT db_pk PRIMARY KEY (db_pk)
@@ -57,7 +56,7 @@ ALTER TABLE public.db OWNER TO postgres;
 -- object: public.db_connection | type: TABLE --
 -- DROP TABLE IF EXISTS public.db_connection CASCADE;
 CREATE TABLE public.db_connection(
-	db_connection_pk smallint NOT NULL,
+	db_connection_pk serial NOT NULL,
 	db_fk smallint,
 	db_connection_username varchar(255),
 	db_connection_password varchar(255),
