caa38c58bb6f661a6da065e85406c34471995ef0
Renier-Rousseau <renier@hisp.org>
2018-01-22 13:54:03 +0200

    Moving and Adding Folders

---
+++ quickFix/bashrcMod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#_   _ _____ ___________   _____  ___
+#| | | |_   _/  ___| ___ \ /  ___|/ _ \
+#| |_| | | | \ `--.| |_/ / \ `--./ /_\ \
+#|  _  | | |  `--. \  __/   `--. \  _  |
+#| | | |_| |_/\__/ / |     /\__/ / | | |
+#\_| |_/\___/\____/\_|     \____/\_| |_/
+#Update the bashrc with the HISP standard
+#Author	: Renier Rousseau
+#Created Date : Jan 2018
+
+#This file needs to be run as root.
+echo "This file needs to be run as root."
+
+wget 
