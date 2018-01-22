20e36d54537aadebf7200f43899cd7e8ae671b9e
Renier-Rousseau <renier@hisp.org>
2018-01-22 14:15:07 +0200

    bashrcMod added
    Added and tested bashrcMod

--- quickFix/bashrcMod.sh
+++ quickFix/bashrcMod.sh
@@ -12,4 +12,23 @@
 #This file needs to be run as root.
 echo "This file needs to be run as root."
 
-wget 
+#Make Workspace
+mkdir /tmp/.working.bashrcMod
+wget -P /tmp/.working.bashrcMod https://raw.githubusercontent.com/hispsa-Infra/scripts/master/quickFix/bashrc.conf
+
+#Updating Skel
+# - backup
+cp /etc/skel/.bashrc /tmp/.working.bashrcMod
+# - overwrite
+cp /tmp/.working.bashrcMod/bashrc.conf /etc/skel/.bashrc
+
+#Moving though users and updating thier bashrc
+users_ls=($(ls /home/))
+for user in "${users_ls[@]}"; do
+  echo "$user - Updating bashrc"
+  cp /etc/skel/.bashrc /home/$user/.bashrc
+  chown $user:$user /home/$user/.bashrc
+done
+
+#Clean Workspace
+rm -R /tmp/.working.bashrcMod
