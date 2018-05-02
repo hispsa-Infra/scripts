edf5fa2ee8ff7333f019f57c4eb7a8312177d66b
Renier-Rousseau <renier@hisp.org>
2018-05-02 12:27:16 +0200

    timestamps added

--- ops/dump.sh
+++ ops/dump.sh
@@ -11,7 +11,7 @@
 
 #TODO test if the users as psql access i.e. is the postgres uesr.
 #TODO either check for more than one argument or add multiple dumps for one command.
-#TODO add an all argument to do a full dump.
+#TODO Standardise time stamps.
 
 
 #Place where the backups will be stored localy
@@ -38,6 +38,8 @@ elif [[ $1 = 'all' ]]; then
   #make the new backup dir.
   all_backup_dir="$backup_dir/$HOSTNAME-$dateinfo-dump"
   touch $all_backup_dir
+  timeinfo=`date '+%Y-%m-%d %H:%M:%S'`
+  echo "$timeinfo > ** Starting Dump of databases **"
   #Move though all the database dumping them all
   for d in $databases; do
     #backuping up all the databases on the system.
@@ -46,11 +48,19 @@ elif [[ $1 = 'all' ]]; then
     /usr/bin/pg_dump --jobs=5 --exclude-table=analytics* --format=directory --file=$backup_dir/$d $d
   done
   #compressing the file
-  echo "dump completed - compressing file"
+  timeinfo=`date '+%Y-%m-%d %H:%M:%S'`
+  echo "$timeinfo > ** Dump Completed - Compressing File **"
   7z a -mx=1 $all_backup_dir.7z $all_backup_dir
 
+  #copy the dump to the dump server.
+  timeinfo=`date '+%Y-%m-%d %H:%M:%S'`
+  echo "$timeinfo > ** Compressing Completed - Moveing File **"
+  scp -P 722 $all_backup_dir.7z dump@10.0.1.245
+
   #closeing out
-  echo "dump of $1 completed - can be found at $all_backup_dir.7z"
+  timeinfo=`date '+%Y-%m-%d %H:%M:%S'`
+  echo "$timeinfo > ** Dump Completed - File can be found at $all_backup_dir.7z and the dump server **"
+
 else
   #Move though all the databases to ensure that the one requested exsists.
   #flag for error if db does exist
